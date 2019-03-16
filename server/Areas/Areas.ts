1bb3c7d21bf9af838d760245a8487746b5a589a8
Jake Overall <joverall22@gmail.com>
2019-03-16 11:43:13 -0600

    Update Areas.ts

--- server/Areas/Areas.ts
+++ server/Areas/Areas.ts
@@ -1,7 +1,7 @@
 //import and export your Areas here ... Generator will handle this eventually
 
-// import SampleArea from './SampleArea/SampleArea'
+import SampleArea from './SampleArea/SampleArea'
 
 export default [
-  // SampleArea
+  SampleArea
 ]
