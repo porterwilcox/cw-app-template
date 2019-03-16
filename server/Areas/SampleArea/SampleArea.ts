c9525a77521f0b4b1404cee71d8c336d6a0521d5
Jake Overall <joverall22@gmail.com>
2019-03-16 11:42:48 -0600

    Update SampleArea.ts

--- server/Areas/SampleArea/SampleArea.ts
+++ server/Areas/SampleArea/SampleArea.ts
@@ -2,7 +2,7 @@ import { Area, SessionUser, ErrorUnAuthorized } from 'codeworks-starter'
 
 export default new Area({
   name: "Sample",
-  controllersPath: __dirname + "/Controllers",
+  controllersPath: __dirname + "/SampleControllers",
   routerMount: "/Sample",
   middleware: [(req, res, next) => {
     if (!SessionUser().hasAccess("admin")) { return next(new ErrorUnAuthorized()) } // locks an entire area
